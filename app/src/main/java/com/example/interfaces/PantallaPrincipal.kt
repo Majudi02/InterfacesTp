4e023b5f32d16321e88a90e7fd30d71025221d08
Mateo <mateodinu02@gmail.com>
2023-11-17 20:31:02 -0300

    Pantalla selecionar macota hecha

---
+++ app/src/main/java/com/example/interfaces/PantallaPrincipal.kt
@@ -0,0 +1,29 @@
+package com.example.interfaces
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.example.interfaces.ui.theme.InterfacesTheme
+
+class PantallaPrincipal: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            InterfacesTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    
+                    Text(text = "Hola")
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/interfaces/PantallaSelecionarMascota.kt
+++ app/src/main/java/com/example/interfaces/PantallaSelecionarMascota.kt
@@ -1,12 +1,22 @@
 package com.example.interfaces
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -21,11 +31,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.interfaces.ui.theme.InterfacesTheme
 
-class PantallaSelecionarMascota: ComponentActivity() {
+class PantallaSelecionarMascota : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -34,8 +50,8 @@ class PantallaSelecionarMascota: ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Text(text = "hola")
-                    Select()
+                    // Select()
+                    SelecionarMascota()
 
                 }
             }
@@ -43,6 +59,116 @@ class PantallaSelecionarMascota: ComponentActivity() {
         }
     }
 
+    @Composable
+    fun SelecionarMascota() {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Row {
+                Image(
+                    painterResource(id = R.drawable.perfil),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(90.dp)
+                        .padding(start = 15.dp)
+                )
+                Text(
+                    text = "Bienvenido Rigoberto",
+                    modifier = Modifier.padding(start = 5.dp, top = 25.dp),
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.titleLarge
+                )
+            }
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Text(
+                text = "Seleccione su Mascota",
+                modifier = Modifier
+                    .fillMaxWidth(),
+                //   .padding(start = 45.dp, top = 23.dp),
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleLarge,
+                color = colorResource(id = R.color.AzulApp)
+            )
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                        startActivity(
+                            Intent(
+                                this@PantallaSelecionarMascota,
+                                PantallaPrincipal::class.java
+                            )
+                        )
+                    }) {
+                Image(
+                    painterResource(id = R.drawable.perro_perfil),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(180.dp)
+                        .padding(start = 10.dp)
+
+                )
+                Column {
+                    Text(
+                        text = "Nombre", modifier = Modifier.padding(start = 15.dp, top = 25.dp),
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = "Ultima Visita: 25/10/2023",
+                        modifier = Modifier.padding(start = 15.dp, top = 10.dp),
+                        fontSize = 13.5.sp
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                        startActivity(
+                            Intent(
+                                this@PantallaSelecionarMascota,
+                                PantallaPrincipal::class.java
+                            )
+                        )
+                    }) {
+                Image(
+                    painterResource(id = R.drawable.gato_perfil),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(180.dp)
+                        .padding(start = 10.dp)
+
+                )
+                Column {
+                    Text(
+                        text = "Nombre", modifier = Modifier.padding(start = 15.dp, top = 25.dp),
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = "Ultima Visita: 10/6/2023",
+                        modifier = Modifier.padding(start = 15.dp, top = 10.dp),
+                        fontSize = 13.5.sp
+                    )
+                }
+            }
+
+        }
+
+    }
 
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
@@ -66,7 +192,7 @@ class PantallaSelecionarMascota: ComponentActivity() {
                 TextField(
                     value = selectedSpecialty,
                     onValueChange = {},
-                    label={ Text(text = "Espacialidad")},
+                    label = { Text(text = "Espacialidad") },
                     readOnly = true,
                     trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
                     modifier = Modifier.menuAnchor() // Este modificador hará que las opciones aparezcan hacia abajo
